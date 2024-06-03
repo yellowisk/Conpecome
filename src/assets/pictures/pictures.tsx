b781886cdc68d09fb23f249d43a8d0aedc8c2789
yellowisk <oheitoralmeida2013@gmail.com>
2024-06-03 20:58:57 -0300

    feat: add png pictures

--- src/assets/pictures/pictures.tsx
+++ src/assets/pictures/pictures.tsx
@@ -1,7 +1,8 @@
-import pacoca from "@assets/pacoca.jpg"
-import toddynho from "@assets/toddynho.jpg"
-import valsa from "@assets/valsa.webp"
-import cupnoodles from "@assets/cup-noodles.webp"
+import pacoca from "@assets/pacoca.png"
+import toddynho from "@assets/toddynho.png"
+import valsa from "@assets/valsa.png"
+import cupnoodles from "@assets/cup-noodles.png"
+import ourobranco from "@assets/ouro-branco.png"
 
 interface IPicture {
     src: string;
@@ -13,5 +14,6 @@ interface IPicture {
   pictures[1] = { src: toddynho.src, name: "Toddynho", price: "2.50"};
   pictures[2] = { src: valsa.src, name: "Sonho de Valsa", price: "3.50"};
   pictures[3] = { src: cupnoodles.src, name: "Cup Noodles", price: "4.50"};
+  pictures[4] = { src: ourobranco.src, name: "Ouro Branco", price: "2.00"};
 
 export default pictures;
