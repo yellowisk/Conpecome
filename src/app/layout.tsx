25c8354060c25f45c43854f56e59d55a8003a5e7
yellowisk <oheitoralmeida2013@gmail.com>
2024-07-16 16:48:01 -0300

    feat: set fonts on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Roboto, Poppins, Anton } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,13 +12,25 @@ export const metadata: Metadata = {
   description: "Conpec's snack HQ",
 };
 
+
+const roboto_init = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '400', '500', '700', '900'],
+  variable: '--font-roboto'
+});
+
+const poppins_init = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '400', '500', '700', '900'],
+  variable: '--font-poppins'
+});
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en" className="">
       <body 
-        className={twMerge(inter.className, )}>
+        className={twMerge(inter.className, `${roboto_init.variable} ${poppins_init.variable}`)}>
           <ThemeProvider 
             attribute="class"
             defaultTheme="light"
