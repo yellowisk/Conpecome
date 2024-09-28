55357c85300b56f2272a1963a17d69bb93709145
yellowisk <oheitoralmeida2013@gmail.com>
2024-09-28 15:15:45 -0300

    feat: add zod to forms

--- src/app/(pages)/adm/login/page.tsx
+++ src/app/(pages)/adm/login/page.tsx
@@ -1,48 +1,74 @@
 "use client"
-import pictures  from '@/assets/pictures/pictures';
 import { Toaster, toast } from 'sonner';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 
 import { useRouter } from 'next/navigation';
 
 import { collection, getDocs } from 'firebase/firestore';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { db, auth } from '@firebase/firebase-config';
-import { User, Product } from '@/firebase/schema/entities';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '@firebase/firebase-config';
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+    email: z.string().email({ message: "Email inválido!" }).min(1),
+    password: z.string().min(8, { message: "Sua senha precisa ter no mínimo 8 caracteres!" }),
+  });
+  
+type FormFields = z.infer<typeof schema>;
+  
 
 export default function Admin() {
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const router = useRouter();
 
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors, isSubmitting },
+      } = useForm<FormFields>({
+        resolver: zodResolver(schema),
+      });
 
-    const handleSignIn = async () => {
+      const onSubmit: SubmitHandler<FormFields> = async (data) => {
         try {
-            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const userCredential = await signInWithEmailAndPassword(auth, data.email, data.password);
             const user = {...userCredential.user, timeout: Date.now() + (1000 * 60 * 30)};
             if (user) {
                 localStorage.setItem('user', JSON.stringify(user));
                 toast.success('Marcha!!!');
                 setTimeout(() => {
                     router.push('/adm/products');
-                }, 1000);
+                }, 2000);
             }
         } catch (error) {
-            toast.error('Eita, deu ruim! Cê é do CACo?');
+            toast.error('Eita, deu ruim!');
         }
-    }
+      };
 
 
     return (
-
-        <div className='flex flex-col justify-center space-y-3 items-center h-screen bg-gradient-to-r from-orange-strong orange-linear to-orange-serene'>
-            <h1 className='text-4xl text-tertiary uppercase select-none font-semibold'>ÁREA ADMIN</h1>
-            <input type="email" onChange={(e) => setEmail(e.target.value)} className='rounded-xl px-2 w-2/6 font-extrabold text-lg py-3 text-orange-600'/>
-            <input type="password" onChange={(e) => setPassword(e.target.value)} className='rounded-xl px-2 w-2/6 font-extrabold text-lg py-3 text-orange-600'/>
-            <Button onClick={handleSignIn} variant='orange' transition='shadow' className='rounded-xl font-extrabold text-lg text-tertiary py-6 select-none'>Logar</Button>
-            <Toaster position="bottom-left" richColors closeButton />
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)} className='login flex flex-col justify-center space-y-3 items-center h-screen bg-gradient-to-r from-orange-strong orange-linear to-orange-serene'>
+                <h1 className='text-4xl text-tertiary uppercase select-none font-semibold'>ÁREA ADMIN</h1>
+                {errors.email && (
+                    <p className="text-slate-200">{errors.email.message}</p>
+                )}
+                <input {...register("email")} type="text" placeholder="Email" className='rounded-xl px-2 w-2/6 font-extrabold text-lg py-3 text-orange-600'/>
+                {errors.password && (
+                    <p className="text-slate-200">{errors.password.message}</p>
+                )}
+                <input {...register("password")} type="password" placeholder={"Password"} className='rounded-xl px-2 w-2/6 font-extrabold text-lg py-3 text-orange-600' />
+                <Button disabled={isSubmitting} type="submit" variant='orange' transition='shadow' className='rounded-xl font-extrabold text-lg text-tertiary py-6 select-none'>
+                    {isSubmitting ? "Carregando..." : "Logar"}
+                </Button>
+                {errors.root && <p className="text-red-500">{errors.root.message}</p>}
+            </form>
+            <Toaster position="bottom-left" richColors closeButton />  
         </div>
     );
 }
