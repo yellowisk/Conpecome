66f05411b49857d0daa86778c8ef014ddf357263
yellowisk <oheitoralmeida2013@gmail.com>
2024-09-28 12:25:40 -0300

    chore: add @emotions/react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 import { Button } from '@/components/ui/button';
 import { Sandwich } from 'lucide-react';
-import { useState, useEffect } from 'react';
 
 export default function App() {
   return (
-    <div className={`flex flex-col space-y-6 flex-1 items-center justify-center h-screen bg-orange-600 text-zinc-200`}>
+    <div className="flex flex-col space-y-6 flex-1 items-center justify-center h-screen bg-orange-600 text-zinc-200">
       <div className="flex flex-row items-center justify-center gap-2 text-slate-50 hover:drop-shadow-glow transition duration-300">
         <Sandwich className="h-16 w-16" />
         <h1 className="text-4xl uppercase caret-transparent select-none font-semibold">Conpecome</h1>
