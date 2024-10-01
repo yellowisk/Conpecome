4f0b80feb0343f8c780b465c5bd29c178d0e2457
mrRiqueRique <riquemribeiro2005@gmail.com>
2024-10-01 17:41:51 -0300

    feat: giving wrong

--- src/app/(pages)/log/page.tsx
+++ src/app/(pages)/log/page.tsx
@@ -9,29 +9,47 @@ import { ArrowLeft } from 'lucide-react';
 import ImageContent from '@/components/ui/image';
 import conpec from '@assets/conpeclogo.png';
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+    name: z.string().max(100, { message: "Nome gigante!" }).min(1, {message: "Insira seu nome!"}),
+    email: z.string().email({ message: "Email inválido!"} ).min(1, { message: "Insira seu email!"}),
+  });
+  
+type FormFields = z.infer<typeof schema>;
+
 export default function Log() {
 
-    const [email, setEmail] = useState<string>('');
     const router = useRouter();
 
-    const saveInput = () => {
+    const saveInput = (email: string) => {
 
-        if (!email) {
-            toast.warning('Email inválido');
-            return;
-        }
-
-        addClientData(email, new Date().toLocaleString(), JSON.parse(localStorage.getItem('items') || '[]'))
-        const data = JSON.parse(localStorage.getItem('items') || '[]')
-        data.map((item: SheetData) => {
-            updateQuantityById(item.id, item)
-        })
-        localStorage.removeItem('items')
+        console.log("AAAAAA")
         toast.success('Marcha, bom ao mosso!')
         setTimeout(() => {
             router.push('/')
         }, 3000);
     }
+
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors, isSubmitting },
+      } = useForm<FormFields>({
+        resolver: zodResolver(schema),
+      });
+
+      const onSubmit: SubmitHandler<FormFields> = async (data) => {
+        console.log("a")
+        try {
+            saveInput(data.email)
+        } catch (error) {
+            toast.error('Eita, deu ruim!');
+        }
+      };
 
 
     return (
@@ -44,19 +62,27 @@ export default function Log() {
                 <ImageContent src={conpec.src} alt='logo' className='size-16'></ImageContent>
                 </div>
             </div>
-            <div className='flex flex-col justify-center border-3 border-orange-linear h-4/5'>
+            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col justify-center border-3 border-orange-linear h-4/5'>
                 <h1 className='text-4xl caret-transparent select-none font-semibold text-orange-coquelicot'>Informações</h1>
                 
                 <h1 className=' caret-transparent select-none font-semibold text-orange-linear text-base'>Nome</h1>
-                <input type="email" placeholder='Inserir nome' onChange={(e) => setEmail(e.target.value)}
+                <input {...register("name")} type="email" placeholder='Inserir nome'
                 className='rounded-xl px-2 font-extrabold text-lg py-3 text-orange-600 w-full placeholder-orange-pale'/>
+                {errors.name && (
+                    <p className="text-slate-200">{errors.name.message}</p>
+                )}
 
                 <h1 className=' caret-transparent select-none font-semibold text-orange-linear text-base'>Email</h1>
-                <input type="email" placeholder='Inserir email' onChange={(e) => setEmail(e.target.value)} 
+                <input {...register("email")}  type="email" placeholder='Inserir email' 
                 className='rounded-xl px-2 font-extrabold text-lg py-3 text-orange-600 w-full placeholder-orange-pale'/>
-            </div>
+                {errors.email && (
+                    <p className="text-slate-200">{errors.email.message}</p>
+                )}
+            </form>
             <div>
-            <Button onClick={() => saveInput()} variant='orange' transition='shadow' className='rounded-xl font-extrabold text-lg text-tertiary py-6 select-none'>Enviar</Button>
+            <Button disabled={isSubmitting} variant='orange' transition='shadow' className='rounded-xl font-extrabold text-lg text-tertiary py-6 select-none'>
+            {   isSubmitting ? "Carregando..." : "Enviar"}
+            </Button>
             </div>
             <Toaster position="bottom-left" richColors closeButton />
         </div>
