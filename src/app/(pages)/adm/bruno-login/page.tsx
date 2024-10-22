7f99b093f396a627033fc86e5ccd77fce38b37c0
Bruno <b281129@dac.unicamp.br>
2024-10-22 15:58:11 -0300

    Pagina de Login de ADM

--- src/app/(pages)/adm/bruno-login/page.tsx
+++ src/app/(pages)/adm/bruno-login/page.tsx
@@ -1,25 +1,62 @@
+"use client"
+import { Button } from "@mui/material";
+import Image from "next/image";
+import { ArrowLeft, ComponentIcon} from "lucide-react";
+import React, { useState } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
+import conpec from '@/assets/conpeclogo.png'
+
 export default function logAdmin() {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+      setShowPassword((prev) => !prev);
+    };
 
     return(
         <div className="flex flex-col bg-orange-seashell h-screen w-screen"> {/*Div que contem a telat toral*/}
-            <div className="flex flex-col border-3 border-blue-900 w-full h-1/3">
+            <div className="flex flex-col items-center w-full h-1/3 pt-3">
+                <div className="flex flex-row w-1/2 h-1/3 border-10 border-blue-900 items-center">
+                    <div className="w-1/2 pl-5">
+                        <ArrowLeft className='text-red-imperial font-bold' size={34}></ArrowLeft> {/*Essa seta tem que fazer algo*/}    
+                    </div>
+                    <div className="flex flex-row w-1/2 justify-end pr-5">
+                        <Image src={conpec.src} alt='logo' width={100} height= {100} className='size-16'></Image>
+                    </div>
+                </div>
+                <h1 className="flex flex-row  h-1/3 font-poppins text-orange-strong text-3xl font-bold justify-center items-center">
+                    Login
+                </h1>
             </div>
-            <div className="flex flex-col items-center border-3 border-blue-900 w-full h-1/3">
-                <div className="border-2 border-red-500 w-1/2 h-1/3"> {/*nome*/} 
-                    <div className="flex flex-row justify-start w-fit h-2/5 text-orange-strong text border-1 font-poppins font-bold text-base ml-5">
+            <div className="flex flex-col items-center w-full h-1/3">
+                <div className=" w-1/2 h-1/3 pb-3 " > {/*nome*/} 
+                    <div className="flex flex-row justify-start w-fit h-2/5 text-orange-linear text font-poppins font-bold text-base ml-5 pt-2">
                     Nome
-                    </div>
-                    
-                    <div>
+                    </div>  
+                    <input type="word" placeholder="Insira seu nome" className="flex flex-row w-full h-3/5 text-sm bg-background border-orange-strong border-2 rounded-full stroke-orange-strong border-opacity-40 justify-start pl-3 font-poppins font-bold text-orange-strong text-opacity-35 items-center focus:outline-none focus:ring-2 focus:ring-orange-strong focus" ></input>
 
+                </div>
+                <div className=" w-1/2 h-1/3 pb-3"> {/*email*/}
+                    <div className="flex flex-row justify-start  w-fit h-2/5 text-orange-linear text border-1 font-poppins font-bold text-base ml-5 pt-2">Email</div>
+                    <input type="word" placeholder="Insira seu email" className="flex flex-row w-full h-3/5 text-sm bg-background border-orange-strong border-2 rounded-full stroke-orange-strong border-opacity-40 justify-start pl-3 font-poppins font-bold text-orange-strong text-opacity-35 items-center focus:outline-none focus:ring-2 focus:ring-orange-strong focus" ></input>
+                </div>
+                <div className=" w-1/2 h-1/3 pb-3"> {/*senha*/}
+                    <div className="flex flex-row justify-start w-fit h-2/5 text-orange-linear text border-1 font-poppins font-bold text-base ml-5 pt-2">Senha</div>
+                        <div className="flex flex-row justify-end items-center h-3/5">
+                            <input type={showPassword ? "text" : "password"} id="password" placeholder="Insira sua senha" className="flex flex-row w-full h-full text-sm bg-background border-orange-strong border-2 rounded-full stroke-orange-strong border-opacity-40 justify-start pl-3 font-poppins font-bold text-orange-strong text-opacity-35 items-center focus:outline-none focus:ring-2 focus:ring-orange-strong focus"/>
+                            <div className="absolute flex items-center pr-5 cursor-pointer" onClick={togglePasswordVisibility}>
+                                {showPassword ? <EyeOff className="text-orange-strong" /> : <Eye className=" text-orange-strong" />}
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="border-2 border-red-500 w-1/2 h-1/3"> {/*email*/}
-                </div>
-                <div className="border-2 border-red-500 w-1/2 h-1/3"> {/*senha*/}
-                </div>
-            </div>
-            <div className="flex flex-col border-3 border-blue-900 w-full h-1/3">
+            <div className="flex flex-row justify-center items-center w-full h-1/3">
+                <Button className="w-1/5 h-1/6 bg-orange-strong rounded-full text-background font-poppins font-bold text-sm">
+                    Entrar
+                </Button>
+
+                
             </div>
         </div>
     );
